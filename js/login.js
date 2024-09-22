9159606ecfec96b3d668d8e40f2c229e0ca913a5
Ridwanul-hoque <rhr06junhoque@gmail.com>
2024-09-23 01:00:40 +0600

    second page

--- js/login.js
+++ js/login.js
@@ -2,5 +2,17 @@ document.getElementById('btn-login').addEventListener('click',function(event){
     event.preventDefault()
     console.log("CLICKED")
     const phoneNumber = document.getElementById('phone-number').value
+    const pinNumber = document.getElementById('pin-number').value
     console.log(phoneNumber)
+    console.log(pinNumber)
+    
+    // this temporary way
+    if(phoneNumber === '5' && pinNumber === '1234'){
+
+        console.log('logged in');
+        window.location.href = '/home.html'
+    }
+    else{
+        alert('wrong')
+    }
 })
