7bdb6a63c73741567d55adecf5737cdcedc7d9ab
Ridwanul-hoque <rhr06junhoque@gmail.com>
2024-09-23 09:24:18 +0600

    added the neBalance

--- js/home.js
+++ js/home.js
@@ -1,1 +1,24 @@
-console.log("added")
+document.getElementById('btn-add').addEventListener('click',function(event){
+    event.preventDefault()
+
+    const addMoney = document.getElementById('add-money').value
+    const pinNumber =document.getElementById('pin-number').value
+
+    if (pinNumber ==='1234'){
+        console.log('adding money')
+        
+        const balance = document.getElementById('bank-balance').innerText
+        console.log(balance)
+
+        const addMoneyNumber = parseFloat(addMoney)
+        const balanceNumber = parseFloat(balance)
+        const newBalance = addMoneyNumber + balanceNumber
+        console.log(newBalance)
+
+        document.getElementById('bank-balance').innerText =newBalance
+
+    }
+    else{
+        alert("nope")
+    }
+})
